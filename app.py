c380f6d5974e1326ae83295ba5921c4e094972ee
Nicholas Chua <nickchuasl@gmail.com>
2022-01-23 14:36:35 +1100

    updates

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/predict", methods = ['POST'])
+@app.route("/", methods = ['POST'])
 def predict():
 
     int_features = [float(x) for x in request.form.values()]
